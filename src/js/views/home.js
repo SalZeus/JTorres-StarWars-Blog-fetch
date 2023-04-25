b4776895bba93c23a4c877c8f20948d5b1a2cc46
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-04-25 03:01:02 +0000

    adding all changes to the base coding files

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext.js"
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+
+	const {store, actions} = useContext(Context);
+
+	useEffect(()=> {
+		actions.fetchStarWars("planets")
+		actions.fetchStarWars("people")
+		actions.fetchStarWars("vehicles")
+	},[]);
+
+		
+
+	
+};
