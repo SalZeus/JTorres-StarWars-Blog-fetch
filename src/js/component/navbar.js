847d58e41b6ea5d7b362a4d7b29c85e4e107cd6a
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-04-26 04:02:22 +0000

    adding Navbar modifications

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,14 +4,23 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div class="logo">
+				<div class="title tc-yellow-400">star</div>
+				<div class="subtitle">A VISUAL GUIDE</div>
+				<div class="title tc-yellow-400">wars</div>
 			</div>
+			<div class="dropstart btn-group ml-auto pe-5">
+				<button type="button" class="btn btn-info dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+					Action
+				</button>
+				<ul class="dropdown-menu">
+					<li><a class="dropdown-item" href="#">Action</a></li>
+					<li><a class="dropdown-item" href="#">Another action</a></li>
+					<li><a class="dropdown-item" href="#">Something else here</a></li>
+					<li><hr class="dropdown-divider"/></li>
+					<li><a class="dropdown-item" href="#">Separated link</a></li>
+				</ul>
+				</div>
 		</nav>
 	);
 };
